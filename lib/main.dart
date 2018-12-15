6604d659891b7235ddc00f6993afd3e17c948f60
dcdevito <ddevito@gmail.com>
2018-12-15 16:19:50 -0500

    added some test images

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 
-main() {
-  //first must attach a widget to the screen
-  runApp(Devpoll());
-}
+void main() => runApp(Devpoll());
+
 
 //classes used to create blueprint for new widgets (objects)
 class Devpoll extends StatelessWidget {
-  build(context) {
+  @override
+  Widget build(BuildContext context) {
     //core root widget() that will be used in all apps
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('DevPoll App'),
         ),
+        body: Card(child: Column(children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text('Food Paradise')
+        
+
+         ] ),),
       ),
     );
   }
