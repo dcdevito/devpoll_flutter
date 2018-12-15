96f778bbdd11071d17e9b7a4409ff83285e98303
dcdevito <ddevito@gmail.com>
2018-12-15 16:37:45 -0500

    added styling and buttons

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(Devpoll());
 
-
 //classes used to create blueprint for new widgets (objects)
 class Devpoll extends StatelessWidget {
   @override
@@ -10,16 +9,28 @@ class Devpoll extends StatelessWidget {
     //core root widget() that will be used in all apps
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('DevPoll App'),
-        ),
-        body: Card(child: Column(children: <Widget>[
-          Image.asset('assets/food.jpg'),
-          Text('Food Paradise')
-        
-
-         ] ),),
-      ),
+          appBar: AppBar(
+            title: Text('DevPoll App'),
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                onPressed: () {},
+                child: Text('Add Product'),
+              ),  
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assets/food.jpg'),
+                    Text('Food Paradise')
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
